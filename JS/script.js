3ccfbfda01434f51ba4132df245c716b4d55ac19
Karan-Tiwari <karantiwari35155@gmail.com>
2026-03-02 19:01:48 +0530

    project updated

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,7 @@ const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle');
 
 // Menu show - hidden
-navToggle.addEventListener('click',() =>{
+navToggle.addEventListener('click', () => {
     navMenu.classList.toggle('show-menu');
     navToggle.classList.toggle('animate-toggle');
 });
@@ -33,19 +33,19 @@ window.addEventListener('scroll', scrollHeader);
 const sections = document.querySelectorAll('section[id]');
 
 const scrollActive = () => {
-    const scrollY =window.pageYOffset;
+    const scrollY = window.pageYOffset;
 
     sections.forEach((current) => {
         const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop,
-        sectionId = current.getAttribute('id'),
-        sectionsClass = document.querySelector(
-            '.nav-menu a[href*=' + sectionId + ']'
-        )
+            sectionTop = current.offsetTop,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector(
+                '.nav-menu a[href*=' + sectionId + ']'
+            )
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             sectionsClass.classList.add('active-link');
-        }else {
+        } else {
             sectionsClass.classList.remove('active-link');
         }
     });
@@ -56,18 +56,18 @@ window.addEventListener('scroll', scrollActive)
 // ---------------- SERVICES SWIPER -----------------
 var servicesSwiper = new Swiper(".services-swiper", {
     spaceBetween: 32,
-     pagination: {
+    pagination: {
         el: '.swiper-pagination',
         clickable: true,
-      },
+    },
 
-      breakpoints: {
+    breakpoints: {
         768: {
-          slidesPerView: 2,
+            slidesPerView: 2,
 
         },
         1208: {
-          slidesPerView: 3,
+            slidesPerView: 3,
         },
     },
 });
@@ -92,15 +92,15 @@ function activeWork() {
     this.classList.add('active-work');
 }
 
-linkwork.forEach((a) => a.addEventListener('click',activeWork));
+linkwork.forEach((a) => a.addEventListener('click', activeWork));
 
 // ----------------- RESUME ------------------
 const accordioItems = document.querySelectorAll('.resume-item');
 
 accordioItems.forEach((item) => {
     const header = item.querySelector('.resume-header'),
-    content = item.querySelector('.resume-content'),
-    icon = item.querySelectorAll('.resume-icon i');
+        content = item.querySelector('.resume-content'),
+        icon = item.querySelectorAll('.resume-icon i');
 
     header.addEventListener('click', () => {
         const isOpen = item.classList.toggle('accordion-open');
@@ -109,7 +109,7 @@ accordioItems.forEach((item) => {
         icon.className = isOpen ? 'ri-subtract-line' : 'ri-add-line';
 
         accordioItems.forEach((otherItem) => {
-            if(otherItem !== item && otherItem.classList.contains('accordion-open')){
+            if (otherItem !== item && otherItem.classList.contains('accordion-open')) {
                 otherItem.querySelector('.resume-content').style.height = '0';
                 otherItem.querySelector('.resume-icon i').classList = 'ri-add-line';
                 otherItem.classList.remove('accordion-open');
@@ -122,38 +122,38 @@ accordioItems.forEach((item) => {
 
 // ----------------- EMAIL JS ------------------------
 const contactForm = document.getElementById('contact-form'),
-contactName = document.getElementById('contact-name'),
-contactEmail = document.getElementById('contact-email'),
-contactSubject = document.getElementById('contact-subject'),
-contactMessage = document.getElementById('contact-message'),
-message = document.getElementById('message');
+    contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactSubject = document.getElementById('contact-subject'),
+    contactMessage = document.getElementById('contact-message'),
+    message = document.getElementById('message');
 
 const sendEmail = (e) => {
     e.preventDefault();
 
-    if(contactName.value === '' || contactEmail.value === '' || contactSubject.value === '' || contactMessage.value === '') {
+    if (contactName.value === '' || contactEmail.value === '' || contactSubject.value === '' || contactMessage.value === '') {
         message.classList.remove('color-first');
         message.classList.add('color-red');
         message.textContent = 'Write all the input field';
 
         setTimeout(() => {
             message.textContent = '';
-        },3000);
+        }, 3000);
     } else {
-       emailjs.sendForm('service_60svlet', 'template_vp1otda', '#contact-form', '4UdMmp7yno_PLg_ip')
-        .then((response) => {
-            message.classList.add('color-first');
-            message.textContent = 'Message sent';
+        emailjs.sendForm('service_60svlet', 'template_vp1otda', '#contact-form', '4UdMmp7yno_PLg_ip')
+            .then((response) => {
+                message.classList.add('color-first');
+                message.textContent = 'Message sent';
 
-             setTimeout(() => {
-            message.textContent = '';
-        },5000);
+                setTimeout(() => {
+                    message.textContent = '';
+                }, 5000);
 
-        },
-        (error) => {
-           alert('OOPs! SOMETHING WENT WRONG...', error);
-         }
-        );
+            },
+                (error) => {
+                    alert('OOPs! SOMETHING WENT WRONG...', error);
+                }
+            );
         contactName.value = '';
         contactEmail.value = '';
         contactSubject.value = '';
@@ -165,8 +165,8 @@ contactForm.addEventListener('submit', sendEmail);
 
 /*------------------- STYLE SWITCHER --------------------- */
 const styleSwitcher = document.getElementById('style-switcher'),
-switcherToggle = document.getElementById('switcher-toggle'),
-switcherClose = document.getElementById('switcher-close');
+    switcherToggle = document.getElementById('switcher-toggle'),
+    switcherClose = document.getElementById('switcher-close');
 
 // Switcher show
 switcherToggle.addEventListener('click', () => {
@@ -183,12 +183,12 @@ const colors = document.querySelectorAll('.style-switcher-color');
 
 colors.forEach((color) => {
     color.onclick = () => {
-        const activeColor =color.style.getPropertyValue('--hue');
+        const activeColor = color.style.getPropertyValue('--hue');
 
         colors.forEach((c) => c.classList.remove('active-color'));
         color.classList.add('active-color');
 
-        document.documentElement.style.setProperty('--hue',activeColor);
+        document.documentElement.style.setProperty('--hue', activeColor);
     };
 });
 
@@ -198,7 +198,7 @@ let currentTheme = 'light';
 document.body.classList = currentTheme;
 
 document.querySelectorAll('input[name="body-theme"]').forEach((input) => {
-    input.addEventListener('change', ()=> {
+    input.addEventListener('change', () => {
         currentTheme = input.value;
         document.body.classList = currentTheme;
     });
